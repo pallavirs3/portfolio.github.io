124fd8ce0476dc07021c585f53974f11131d1dee
Pallavi4740@gmail.com <pallavigowda4123@gmail.com>
2024-07-14 17:09:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed(".text", {
+    strings: ["Ethical Hacking" , "Cybersecurity" , "Networking"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+ 
